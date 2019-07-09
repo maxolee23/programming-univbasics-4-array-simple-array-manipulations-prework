8568f78f0901425008f4b1e127e7748efaea2a55
Max Lee <maxolee23@gmail.com>
2019-07-09 19:24:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat (array1, array2)
   array1.concat(array2)
 end
 
-def using_insert (array, element)
-  array.insert(element, [4])
+def using_insert (array, new_element)
+  array.insert(4, new_element)
 end
 
 def using_uniq (array)
